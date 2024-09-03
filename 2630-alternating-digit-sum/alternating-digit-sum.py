900f1953118af8bf548a3b670d64fe163d3eb0c7
gokul-cce <90777702+gokul-cce@users.noreply.github.com>
2024-09-03 22:36:14 +0530

    Time: 34 ms (60.62%) | Memory: 16.5 MB (29.56%) - LeetSync

--- 2630-alternating-digit-sum/alternating-digit-sum.py
+++ 2630-alternating-digit-sum/alternating-digit-sum.py
@@ -1,12 +1,10 @@
 class Solution:
     def alternateDigitSum(self, n: int) -> int:
         total = 0
-        st = str(n)
-        for i in range(len(st)):
-            if i % 2:
-                total -= int(st[i])
-            else:
-                total += int(st[i])
+        flag = 1
+        for i in str(n):
+            total += (flag * int(i))
+            flag *= -1
         return total
 
         
