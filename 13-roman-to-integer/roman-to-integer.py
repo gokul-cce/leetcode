12452118149d0f7b2c0a18f5dd7e8392548d4ece
gokul-cce <90777702+gokul-cce@users.noreply.github.com>
2024-09-22 20:56:56 +0530

    Time: 39 ms (89.51%) | Memory: 16.6 MB (7.83%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,26 +1,16 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         dic = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        total = 0
-        for i in range(len(s)-1,-1,-1):
-            if s[i] == 'I' and (i != len(s)-1) and (s[i+1] == 'V' or s[i+1] == 'X'):
-                total = total - dic[s[i+1]] + (dic[s[i+1]]-1)
-                # print(total)
-                continue
-            elif s[i] == 'X' and (i != len(s)-1) and (s[i+1] == 'L' or s[i+1] == 'C'):
-                total = total - dic[s[i+1]] + (dic[s[i+1]]-10)
-                # print(total)
-                continue
-            elif s[i] == 'C' and (i != len(s)-1) and (s[i+1] == 'D' or s[i+1] == 'M'):
-                total = total - dic[s[i+1]] + (dic[s[i+1]]-100)
-                # print(total)
-                continue
-            
-            total += dic[s[i]]
-            # print(total)
-            
-
-        return total
+        tot_sum = 0
+        n = len(s)
+        
+        for i in range(n):
+            if i < n-1 and dic[s[i]] < dic[s[i+1]]:
+                tot_sum -= dic[s[i]]
+            else:
+                tot_sum += dic[s[i]]
+        
+        return tot_sum
 
 
 
