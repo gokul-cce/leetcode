91b9b06e89dc2770072b281edee8f90ee96a8b7c
gokul-cce <90777702+gokul-cce@users.noreply.github.com>
2024-09-25 20:49:10 +0530

    Time: 39 ms (49.68%) | Memory: 16.6 MB (35.84%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -9,29 +9,22 @@ class Solution:
         temp2 = list2
         dummy = ListNode(-1)
         res = dummy
-        while temp1 != None or temp2 != None:
-            if temp1 and temp2:
-                if temp1.val < temp2.val:
-                    new_node = ListNode(temp1.val)
-                    res.next = new_node
-                    res = res.next
-                    temp1 = temp1.next
-                else:
-                    new_node = ListNode(temp2.val)
-                    res.next = new_node
-                    res = res.next
-                    temp2 = temp2.next
-                continue
-            if temp1:
-                new_node = ListNode(temp1.val)
-                res.next = new_node
+        while temp1 != None and temp2 != None:
+            if temp1.val < temp2.val:
+                res.next = ListNode(temp1.val)
                 temp1 = temp1.next
-                res = res.next
-            if temp2:
-                new_node = ListNode(temp2.val)
-                res.next = new_node
+            else:
+                res.next = ListNode(temp2.val)
                 temp2 = temp2.next
-                res = res.next
+            res = res.next         
+        while temp1:
+            res.next = ListNode(temp1.val)
+            temp1 = temp1.next
+            res = res.next
+        while temp2:
+            res.next = ListNode(temp2.val)
+            temp2 = temp2.next
+            res = res.next
         return dummy.next
         
         
