4847573ab3f180927075c958b81aaf0bb42104fc
GOKUL P <90777702+gokul-cce@users.noreply.github.com>
2024-10-13 19:41:49 +0530

    Time: 36 ms (63.15%) | Memory: 16.7 MB (31.94%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,10 +1,14 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        expected = []
-        for i in nums:
-            if i != val:
-                expected.append(i)
-        n = len(expected)
-        for j in range(n):
-            nums[j] = expected[j]
-        return n
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            if nums[left] != val:
+                left += 1
+            elif nums[right] == val:
+                right -= 1
+            else:
+                nums[left],nums[right] = nums[right],nums[left]
+                left += 1
+                right -= 1
+        return left
