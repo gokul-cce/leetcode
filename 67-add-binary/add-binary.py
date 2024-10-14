be2e9208915eaf65fa0c684fdb69c3e0c63270f8
GOKUL P <90777702+gokul-cce@users.noreply.github.com>
2024-10-14 20:04:06 +0530

    Time: 39 ms (43.76%) | Memory: 16.7 MB (28.02%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,12 +1,12 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        def binary(num):
-            total = 0
-            index = 0
-            for i in range(len(num)-1,-1,-1):
-                if num[i] == '1':
-                    total += int(num[i]) * (2**(index))
-                index += 1
-            return total
-        res = binary(a) + binary(b)
-        return str(bin(res))[2:]
+        # def binary(num):
+        #     total = 0
+        #     index = 0
+        #     for i in range(len(num)-1,-1,-1):
+        #         if num[i] == '1':
+        #             total += int(num[i]) * (2**(index))
+        #         index += 1
+        #     return total
+        # res = int(a,2) + int(b,2)
+        return str(bin(int(a,2) + int(b,2)))[2:]
