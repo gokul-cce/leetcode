eda7681dd717b401c148c1be6d457c2966345d8c
GOKUL P <90777702+gokul-cce@users.noreply.github.com>
2025-01-12 20:24:22 +0530

    Time: 7 ms (23.44%) | Memory: 18.7 MB (9.97%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -5,8 +5,9 @@ class Solution:
         """
         m = len(matrix)
         n = len(matrix[0])
-        row = [0] * m
-        col = [0] * n
+        col0 = 1
+        # row = [0] * m
+        # col = [0] * n
         # def markrow(i):
         #     for j in range(n):
         #         if matrix[i][j] != 0:
@@ -20,13 +21,30 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 0:
-                    row[i] = 1
-                    col[j] = 1
+                    # row[i] = 1
+                    matrix[i][0] = 0
+                    # col[j] = 1
+                    if j != 0:
+                        matrix[0][j] = 0
+                    else:
+                        col0 = 0
+                    
 
-        for i in range(m):
-            for j in range(n):
-                if row[i] or col[j]:
+        for i in range(1,m):
+            for j in range(1,n):
+                # if row[i] or col[j]:
+                #     matrix[i][j] = 0
+                if (matrix[i][0] == 0) or (matrix[0][j] == 0):
                     matrix[i][j] = 0
+        
+        if matrix[0][0] == 0:
+            for j in range(1,n):
+                matrix[0][j] = 0
+
+        if col0 == 0:
+            for i in range(m):
+                matrix[i][0] = 0
+
         return matrix
     
 
