3c90f69c43ea1091adb682b8df61f94dc0cbdb08
GOKUL P <90777702+gokul-cce@users.noreply.github.com>
2025-02-27 23:28:07 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (24.58%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,13 +1,22 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+
         dic = {}
         for i in range(len(nums)):
-            dic[nums[i]] = i
-        print(dic)
-        for j in range(len(nums)):
-            compliment = target - nums[j]
-            if compliment in dic and dic[compliment] != j:
-                return [j,dic[compliment]]
+            rem = target - nums[i]
+            if rem in dic:
+                return [dic[rem],i]
+            dic[nums[i]] = i 
+        
+        # for i in range(len(nums)):
+        #     dic[nums[i]] = i
+        # print(dic)
+        # for j in range(len(nums)):
+        #     compliment = target - nums[j]
+        #     if compliment in dic and dic[compliment] != j:
+        #         return [j,dic[compliment]]
+
+        
         # for i in range(len(nums)):
         #     compliment = target - nums[i]
         #     if compliment in dic:
